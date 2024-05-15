6c3012a086e92ea98ed884b994b4129240b07f71
nomeodavaadorj <nominerdene.d@mezorn.com>
2024-05-15 16:50:49 +0800

    top musics

--- lib/pages/home/controller/home_controller.dart
+++ lib/pages/home/controller/home_controller.dart
@@ -1,6 +1,61 @@
+import 'dart:developer';
+import 'package:dio/dio.dart' as dio;
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import '../state/home_state.dart';
 
 class HomeController extends GetxController {
+  final state = HomeState();
+
   TextEditingController searchBarController = TextEditingController();
+
+  void searchMusicByMusicName(String musicName) async {
+    var dioClient = Dio();
+    try {
+      dio.Response response = await dioClient.get('http://ws.audioscrobbler.com/2.0/', queryParameters: {
+        'method': 'track.search',
+        'track': musicName,
+        'api_key': state.apiKeyLastFm,
+        'format': 'json',
+        'limit': 30,
+      });
+      log(response.data.toString());
+    } on DioError catch (e) {
+      log('DioError received: ${e.response?.data ?? e.message}');
+    }
+  }
+
+  Future<void> getTopTracks() async {
+    var dioClient = Dio();
+
+    try {
+      dio.Response response = await dioClient.get(
+        'http://ws.audioscrobbler.com/2.0/',
+        queryParameters: {
+          'method': 'geo.getTopTracks',
+          'country': 'Mongolia',
+          'api_key': state.apiKeyLastFm,
+          'format': 'json',
+        },
+      );
+
+      var topTracks = response.data['tracks']['track'];
+      state.topTracks.value = response.data['tracks']['track'];
+
+      log('top tracks: ${response.data['tracks']['track']}');
+      for (var track in topTracks) {
+        log('Track: ${track['name']} by ${track['artist']['name']}');
+      }
+    } on DioError catch (e) {
+      log('DioError received: ${e.response?.data ?? e.message}');
+    }
+  }
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    getTopTracks();
+  }
 }
---
+++ lib/pages/home/view/music_list_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pulse_play_mobile_app/pages/home/controller/home_controller.dart';
+
+class MusicListScreen extends GetWidget<HomeController> {
+  const MusicListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold();
+  }
+}
